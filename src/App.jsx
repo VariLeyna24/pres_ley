9d5cf4514a1d32e4d097a1e6e44d6fc16f9a8b55
VariLeyna24 <leynavarinia.pacheco@studentambassadors.com>
2024-06-14 02:07:51 -0400

    add_chane11

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Leyna Varinia Pacheco Quisbert ",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "leyna.v.p.q@hotmail.com",
+  gitHub: "VariLeyna24",
+  instagram: "leynavarinia",
+  linkedIn: "lleyna-varinia-pacheco-quisbert",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "@LeynaV",
+  youTube: "@leynapacheco2880",
 };
 
 const primaryColor = "#4E567E";
